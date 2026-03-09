bb7ceb99c61a0567d99b76d52a4166aacfbe8974
Analice Coimbra <analicecoimbra2000@gmail.com>
2026-03-08 23:13:23 -0300

    feat/add-exercise-controller
    Separado por pastas está o Service, Repository, Request DTO e ResponseDTO, Controller e  definição de entity.
    
    Habilidades do controller:
    Adicionar exercicio, editar (por id) todos os atributos de exercicio, buscar todos, buscar por id, buscar por nome, buscar por tags, deletar por id

---
+++ src/backend/MayaFisioLumiere/src/main/java/com/example/MayaFisioLumiere/entity/ExerciseEntity.java
@@ -0,0 +1,33 @@
+package com.example.MayaFisioLumiere.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "exercises")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ExerciseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long exercise_ID;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String midiaURL;
+
+    @Column(nullable = false)
+    private String tags;
+
+    @Column(nullable = false)
+    private String description;
+
+}
