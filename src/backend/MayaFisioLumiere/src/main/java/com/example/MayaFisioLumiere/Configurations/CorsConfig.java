0f09f2a66e4e7fe126aa3215cd72dbeda6da3ad9
Analice Coimbra <analicecoimbra2000@gmail.com>
2026-03-15 18:49:42 -0300

    fix/remove-SecurityConfig

--- src/backend/MayaFisioLumiere/src/main/java/com/example/MayaFisioLumiere/Configurations/CorsConfig.java
+++ src/backend/MayaFisioLumiere/src/main/java/com/example/MayaFisioLumiere/Configurations/CorsConfig.java
@@ -18,6 +18,6 @@ public class CorsConfig implements WebMvcConfigurer {
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(false); //MUDAR PARA TRUE ANTES DO COMMIT
+                .allowCredentials(true);
     }
 }
