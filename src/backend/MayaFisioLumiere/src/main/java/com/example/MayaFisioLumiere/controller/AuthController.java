9b7307019807d4a93379ea571d5fb21a7185ba77
Analice Coimbra <analicecoimbra2000@gmail.com>
2026-03-14 22:11:53 -0300

    add/AuthController-initial-setup

--- src/backend/MayaFisioLumiere/src/main/java/com/example/MayaFisioLumiere/controller/AuthController.java
+++ src/backend/MayaFisioLumiere/src/main/java/com/example/MayaFisioLumiere/controller/AuthController.java
@@ -1,4 +1,29 @@
 package com.example.MayaFisioLumiere.controller;
 
+import com.example.MayaFisioLumiere.Services.AdminService;
+import com.example.MayaFisioLumiere.entity.AdminEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//LOGIN DE ADMINISTRADORES - FAZER AUTENTICAÇÃO, LOGIN, LOGOUT AQUI
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    @Autowired
+    private AdminService adminService;
+
+    @PostMapping("/admin/login")
+    public AdminEntity login(@RequestBody AdminEntity admin){
+
+        return adminService.loginAdmin(
+                admin.getAdminEmail(),
+                admin.getAdminPassword()
+        );
+    }
+
+    //FAZER LOGOUT QUANDO TIVER JWT, HASH...
 }
