4c00aac3a19eabcf8bc817430cf3c910b07ccd03
Analice Coimbra <analicecoimbra2000@gmail.com>
2026-03-28 20:50:17 -0300

    refactor: change Exercise name to ExerciseURL
    Tinha duas classes chamadas "Exercise", o model e o que pega a URL do exercício, e estava dando problema de identificação entre as pastas

---
+++ src/frontend-app/app/src/main/java/com/example/projeto8/UI/ExerciseURL.java
@@ -0,0 +1,13 @@
+package com.example.projeto8.UI;
+
+public class ExerciseURL {
+
+    String name;
+    String youtubeUrl;
+
+    public ExerciseURL(String name, String youtubeUrl) {
+        this.name = name;
+        this.youtubeUrl = youtubeUrl;
+    }
+
+}
