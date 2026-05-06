c008685395dfe8e97d852c70ca805ee7062f8192
Analice Coimbra <analicecoimbra2000@gmail.com>
2026-05-06 14:22:47 -0300

    add: Check Workout route on WorkoutService

--- src/frontend-app/app/src/main/java/com/example/projeto8/api/workout/WorkoutService.java
+++ src/frontend-app/app/src/main/java/com/example/projeto8/api/workout/WorkoutService.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import com.example.projeto8.model.WorkoutSession;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface WorkoutService {
@@ -14,6 +15,7 @@ public interface WorkoutService {
     @GET("api/workout/patient/{patient_id}")
     Call<List<WorkoutSession>> getWorkoutsByPatient(@Path("patient_id") String patientId);
 
-
-
+    // Rota para dar o "Check" final no treino e atualizar o status do paciente para ATIVO
+    @PUT("api/workout/check/{id}")
+    Call<WorkoutSession> checkWorkout(@Path("id") Long workoutId);
 }
